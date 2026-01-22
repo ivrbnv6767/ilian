94a0c7002c2db574978fbb5857465a9a887ecc7b
ivrbnv6767 <ivrbnv1@gmail.com>
2026-01-22 10:42:12 +0200

    Create App.py

---
+++ App.py
@@ -0,0 +1,9 @@
+st.title("Project")
+name = st.text_input("Enter name")
+if st.button("Search"):
+  if name.strip()==:
+    st.warning("Molq vavedi text")
+  elif not name in alpha():
+    st.warning("Warning!!!")
+  else st.sucess ("Texta e pravilno vaveden")
+
